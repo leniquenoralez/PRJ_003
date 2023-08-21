7fb4403deb36ccfacad92f681a9d6b568caea77f
Lenique Noralez <leniquen_services+github@protonmail.com>
2023-08-21 11:27:41 -0500

    setup socket and bind

--- C/main.c
+++ C/main.c
@@ -1,11 +1,23 @@
-#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BACKLOG 5
 
 char *DIRECTORY = NULL;
 char *ADDRESS = NULL;
 char *LOG_FILE = NULL;
-int PORT = -1;
+int PORT = 8080;
+
+
 int decodeFlags(int argc, char **argv)
 {
     int opt;
@@ -45,19 +57,43 @@ int decodeFlags(int argc, char **argv)
 int main(int argc, char **argv)
 {
     decodeFlags(argc, argv);
-    if (DIRECTORY != NULL)
+
+    int sock;
+    socklen_t length;
+    struct sockaddr_in server;
+
+    memset(&server, 0, sizeof(server));
+
+    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("DIRECTORY: %s\n", DIRECTORY);
+        perror("opening stream socket");
+        exit(EXIT_FAILURE);
+    }
+    server.sin_family = PF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(PORT);
+
+    if (bind(sock, (struct sockaddr *)&server, sizeof(server)) != 0)
+    {
+        perror("binding stream socket");
+        exit(EXIT_FAILURE);
     }
 
-    if (ADDRESS != NULL)
+    length = sizeof(server);
+    if (getsockname(sock, (struct sockaddr *)&server, &length) != 0)
     {
-        printf("ADDRESS: %s\n", ADDRESS);
+        perror("getting socket name");
+        exit(EXIT_FAILURE);
     }
-    if (PORT != -1)
-    {
-        printf("PORT: %d\n", PORT);
-    }
+
+    char ip_address[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(server.sin_addr), ip_address, INET_ADDRSTRLEN);
+
+    printf("Socket has port %d\n", server.sin_port);
+    printf("Socket has address %s\n", ip_address);
+
+    close(sock);
+
     if (LOG_FILE != NULL)
     {
         printf("LOG_FILE: %s\n", LOG_FILE);
