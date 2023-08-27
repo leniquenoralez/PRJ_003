d1d60b57d8b137f22f3c58e9af93a2a606eb9d51
Lenique Noralez <leniquen_services+github@protonmail.com>
2023-08-27 09:04:48 -0500

    fix listener initialization

--- C/main.c
+++ C/main.c
@@ -10,7 +10,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
-
+#include <poll.h>
 #define BACKLOG 5
 
 char *DIRECTORY = NULL;
@@ -25,40 +25,45 @@ int nextAvailableFd(int *client_fds, int maxClients){
     }
     return i;
 }
-int initServer(struct addrinfo hints, struct addrinfo *servinfo, char *port)
-{
+int InitServer() {
     int status;
-    int sockfd;
-    struct addrinfo  *p;
+    int server_fd;
+
+    struct addrinfo  hints;
+    struct addrinfo *server_info;
+    struct addrinfo  *curr_addr_info;
+
     int yes = 1;
-
-    hints.ai_family = AF_UNSPEC;     
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // use IPV4 or IPV6    
     hints.ai_socktype = SOCK_STREAM; 
-    hints.ai_flags = AI_PASSIVE;     
+    hints.ai_flags = AI_PASSIVE; // use localhost
 
-    if ((status = getaddrinfo(NULL, port, &hints, &servinfo)) != 0)
+    if ((status = getaddrinfo(NULL, PORT, &hints, &server_info)) != 0)
     {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
         exit(1);
     }
-    for (p = servinfo; p != NULL; p = p->ai_next)
+    for (curr_addr_info = server_info; curr_addr_info != NULL; curr_addr_info = curr_addr_info->ai_next)
     {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                             p->ai_protocol)) == -1)
+
+        server_fd = socket(curr_addr_info->ai_family, curr_addr_info->ai_socktype, curr_addr_info->ai_protocol);
+
+        if (server_fd == -1)
         {
             perror("server: socket");
             continue;
         }
 
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
         {
             perror("setsockopt");
             exit(1);
         }
 
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        if (bind(server_fd, curr_addr_info->ai_addr, curr_addr_info->ai_addrlen) == -1)
         {
-            close(sockfd);
+            close(server_fd);
             perror("server: bind");
             continue;
         }
@@ -66,23 +71,24 @@ int initServer(struct addrinfo hints, struct addrinfo *servinfo, char *port)
         break;
     }
 
-    freeaddrinfo(servinfo);
+    freeaddrinfo(server_info);
 
-    if (p == NULL)
+    if (curr_addr_info == NULL)
     {
         fprintf(stderr, "server: failed to bind\n");
         exit(1);
     }
 
-    if (listen(sockfd, BACKLOG) == -1)
+    if (listen(server_fd, BACKLOG) == -1)
     {
         perror("listen");
         exit(1);
     }
-    return sockfd;
+    printf("Server Listening on %s 🚀\n\n", PORT);
+    return server_fd;
 }
 
-int decodeFlags(int argc, char **argv)
+int DecodeFlags(int argc, char **argv)
 {
     int opt;
 
@@ -121,14 +127,30 @@ int decodeFlags(int argc, char **argv)
 #define MAX_CLIENTS 10
 int main(int argc, char **argv)
 {
-    decodeFlags(argc, argv);
-    struct addrinfo hints, *servinfo;
-    memset(&hints, 0, sizeof hints);
-    memset(&servinfo, 0, sizeof servinfo);
-    int server_fd = initServer(hints, servinfo, PORT);
-    printf("Sever socket descriptor is %d", server_fd);
-    // TODO:
-    // TODO: Speaks only HTTP/1.0
+    DecodeFlags(argc, argv);
+    int current_client_fd;
+    struct sockaddr_storage client_addr;
+    socklen_t client_addr_len;
+    char data_buffer[256];
+    char client_ip[INET6_ADDRSTRLEN];
+
+    int server_fd = InitServer();
+    if (server_fd == -1)
+    {
+        fprintf(stderr, "error getting listening socket\n");
+        exit(1);
+    }
+
+    int connected_sockets_count = 0;
+    int max_connected_sockets = 5;
+    struct pollfd *poll_fds = malloc(sizeof *poll_fds * max_connected_sockets);
+
+    poll_fds[0].fd = server_fd;
+    poll_fds[0].events = POLL_IN;
+
+    connected_sockets_count = 1;
+
+
     return 0;
 }
 
